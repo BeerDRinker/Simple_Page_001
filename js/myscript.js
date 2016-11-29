0393798b010ebf48d45c6707de67385ea7062299
Paliy Rostyslav <paliy1984@gmail.com>
2016-11-29 15:26:47 +0200

    fixing video fit

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,27 @@
 
 /*jslint vars: true, devel: true*/
 /*global $, jQuery, alert*/
+
+
+$(window).scroll(function () {
+    'use strict';
+
+    var sticky = $('#myNavbar'),
+        scroll = $(window).scrollTop(),
+        navbar = $('.navbar'),
+        top = $('#top');
+
+    if (scroll > 98) {
+
+        sticky.addClass('navbar-fixed-top');
+        navbar.css('background', '#000');
+        top.addClass('hero-fixed-padding');
+
+    } else {
+
+        sticky.removeClass('navbar-fixed-top');
+        navbar.css('background', 'rgba(0, 0, 0, 0.5)');
+        top.removeClass('hero-fixed-padding');
+
+    }
+});
